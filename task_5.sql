4a61d275bbe797db85c8e7e65c783dd4a9c9d1f9
GithaigaNgugi <ngugibrian00@gmail.com>
2024-10-27 16:09:18 +0300

    Add SQL script to insert a single row into the customer table

--- task_5.sql
+++ task_5.sql
@@ -1,10 +1,4 @@
--- Set the database to alx_book_store
 USE alx_book_store;
 
--- Insert a single row into the customers table, update if it already exists
-INSERT INTO Customers (customer_id, customer_name, email, address)
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.')
-ON DUPLICATE KEY UPDATE 
-    customer_name = 'Cole Baidoo', 
-    email = 'cbaidoo@sandtech.com', 
-    address = '123 Happiness Ave.';
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
