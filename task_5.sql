c4732b35f0d6caca96489bc27cc231fde68f3217
GithaigaNgugi <ngugibrian00@gmail.com>
2024-10-27 16:06:34 +0300

    Update SQL script to insert or update a row in the customers table

--- task_5.sql
+++ task_5.sql
@@ -1,6 +1,10 @@
 -- Set the database to alx_book_store
 USE alx_book_store;
 
--- Insert a single row into the customers table
+-- Insert a single row into the customers table, update if it already exists
 INSERT INTO Customers (customer_id, customer_name, email, address)
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.')
+ON DUPLICATE KEY UPDATE 
+    customer_name = 'Cole Baidoo', 
+    email = 'cbaidoo@sandtech.com', 
+    address = '123 Happiness Ave.';
